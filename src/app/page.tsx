6940531ac98100c2e9d9643f031dcb905a0010df
subhradeep10 <subhradeepsamanta2002@gmail.com>
2024-01-23 13:55:34 +0530

    Landing page init

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,9 @@ import MaxWidthWrapper from "@/components/MaxWidthWrapper";
 export default function Home() {
   return (
     <MaxWidthWrapper>
-      <div className="py-20 mx-auto text-center flex flex-col"></div>
+      <div className="py-20 mx-auto text-center flex flex-col items-center max-w-3xl">
+        Your MarketPlace for High Quality Services
+      </div>
     </MaxWidthWrapper>
   );
 }
